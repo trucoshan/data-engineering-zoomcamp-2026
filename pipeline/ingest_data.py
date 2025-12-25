2cb0bdc3bed5d8073b9e564d5b6119cd8051031b
Shantanu Das <shantheartist420@gmail.com>
2025-12-25 19:43:13 +0000

    intermediate update to the pipeline

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -4,6 +4,7 @@
 import pandas as pd
 from tqdm.auto import tqdm
 from sqlalchemy import create_engine, URL
+import click
 
 dtype = {
     "VendorID": "Int64",
@@ -71,19 +72,20 @@ def ingest_data(
 
     print(f"Done ingesting to {target_table}")
 
-def main():
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
 
-    year = 2021
-    month = 1
-    chunksize = 100000
-    pg_user = "root"
-    pg_password = "root"
-    pg_host = "localhost"
-    pg_port = 5432
-    pg_database = "ny_taxi"
-    target_table = "yellow_taxi_data"
+def main(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
 
-    engine = create_engine(f"postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_database}")
+    engine = create_engine(f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}")
     url_prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow"
     url = f"{url_prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz"
 
